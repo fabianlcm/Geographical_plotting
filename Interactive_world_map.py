d43d5752da89bc8fe36d6e6d6b18dbf7416b6e04
Fabian Cabrera <46263609+fabianlcm@users.noreply.github.com>
2019-01-08 20:49:20 +0100

    Update Interactive_world_map.py

--- Interactive_world_map.py
+++ Interactive_world_map.py
@@ -8,9 +8,9 @@ init_notebook_mode(connected=True)
 # function of an orthographic-type map
 def orthographicMap(my_countries, my_title, my_colors):
     map_data = dict(type = 'choropleth',
-                locations = my_locations,
+                locations = my_countries,
                 locationmode = 'country names',
-                z = [1 for country in my_locations],
+                z = [1 for country in my_countries],
                 colorbar = {'title': '{}'.format(my_title)},
                 colorscale=[[0, '{}'.format(my_colors[0])], [1, '{}'.format(my_colors[1])]])
     map_layout = dict(title = '{}'.format(my_title), geo = dict(showframe = True, projection = {'type': 'orthographic'}))
@@ -32,6 +32,4 @@ my_title = 'My list of visited countries'
 # colors in rgb format 
 my_colors = ['rgb(255, 255, 255)', 'rgb(250, 0, 0)']
 
-orthoghraphicMap(my_countries, my_title, my_colors)
-    
-  
+orthographicMap(my_countries, my_title, my_colors)
