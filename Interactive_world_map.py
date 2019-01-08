486be89cb82822b233dd8b2a4678953fe1c796fd
Fabian Cabrera <46263609+fabianlcm@users.noreply.github.com>
2019-01-08 16:23:12 +0100

    Update Interactive_world_map.py

--- Interactive_world_map.py
+++ Interactive_world_map.py
@@ -6,6 +6,32 @@ import matplotlib.pyplot as plt
 import pandas as plt
 %matplotlib inline
 
-def countriesVisited():
+def orthographicMap(my_countries, my_title, my_colors):
+    map_data = dict(type = 'choropleth',
+                locations = my_locations,
+                locationmode = 'country names',
+                z = [1 for country in my_locations],
+                colorbar = {'title': '{}'.format(my_title)},
+                colorscale=[[0, '{}'.format(my_colors[0])], [1, '{}'.format(my_colors[1])]])
+    map_layout = dict(title = '{}'.format(my_title), geo = dict(showframe = True, projection = {'type': 'orthographic'}))
+    
+    my_map = go.Figure(data = [map_data], 
+                       layout = map_layout)
+    
+    iplot(my_map)
+    
+    
+# list of my countries visited
+my_countries = ['Colombia', 'Venezuela', 'Mexico', 'United States', 
+                'The Bahamas', 'United Kingdom', 'Belgium', 'Spain', 
+                'China', 'France', 'Germany', 'Turkey']
+
+# choose title of the map
+my_title = 'My list of visited countries'
+
+# choose colors in rgb format 
+my_colors = ['rgb(255, 255, 255)', 'rgb(250, 0, 0)']
+
+orthoghraphicMap(my_countries, my_title, my_colors)
     
   
